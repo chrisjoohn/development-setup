665e3b01484d5977891e3fab4f6a1e069baf6a35
Chris John <chrisjohnmulingbayan@gmail.com>
2020-08-25 05:12:58 +0800

    Removed copying of tmux-themes command

--- setup.sh
+++ setup.sh
@@ -35,7 +35,6 @@ git clone https://github.com/chrisjoohn/development-setup.git ~/Development
 
 sudo cp ~/Development/development-setup/.vimrc ~/
 sudo cp ~/Development/development-setup/.tmux.conf ~/
-sudo cp -r ~/Development/development-setup/tmux/ ~/
 
 cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 
